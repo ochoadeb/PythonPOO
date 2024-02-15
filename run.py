ec48598b89e3f66db9ff43697a190be7e9e563ee
ochoadeb <84750875+ochoadeb@users.noreply.github.com>
2024-02-15 16:46:52 -0300

    Create run.py

---
+++ run.py
@@ -0,0 +1,21 @@
+def operacion(num1, num2, opcion):
+    if opcion == 1:
+        resultado = num1 + num2
+        print(f"La suma de {num1} y {num2} es: {resultado}")
+    elif opcion == 2:
+        resultado = num1 * num2
+        print(f"El producto de {num1} y {num2} es: {resultado}")
+    else:
+        print("Opción no válida. Por favor, ingrese 1 o 2.")
+
+def main():
+    print("Bienvenido a mi primer programa de mi Curso De POO")
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+
+    opcion = int(input("Seleccione la operación:\n1. Suma\n2. Multiplicación\nIngrese el número de la opción: "))
+
+    operacion_aritmetica(num1, num2, opcion)
+
+if __name__ == "__main__":
+    main()
